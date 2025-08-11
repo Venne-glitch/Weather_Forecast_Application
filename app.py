0d0fd00309a502db915fc3226ab9bf1fe1e200ed
Vennela Kache <Kachevennela46@gmail.com>
2025-08-11 10:19:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.set_page_config(page_title="Weather Dashboard", layout="wide")
 st.title("How's the weather? 🌤️")
 
 # ==== File Path ====
-file_path = r"D:\Deep Learning\weather_forecast_data (1).csv"
+file_path = r"D:\Deep Learning\weather_forecast_data.csv"
 
 if not os.path.exists(file_path):
     st.error(f"File not found at: {file_path}")
@@ -58,3 +58,4 @@ else:
 
     fig.tight_layout()
     st.pyplot(fig)
+
